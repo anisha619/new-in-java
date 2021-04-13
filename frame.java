a28ae6bab00e17d34cce69c3c76b744d13d395e2
anisha619 <82448953+anisha619@users.noreply.github.com>
2021-04-13 17:05:33 +0530

    Create frame.java

---
+++ frame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+class Test
+{
+public static void main(String args[])
+{
+JFrame jf = new JFrame();
+jf.setSize(300,300);
+jf.setVisible(true);
+jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+}
